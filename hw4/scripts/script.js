42119bdeb63f69aa4cfc242ce4ddc35420b365b8
v.kisel <v.kisel@dpc-it.net>
2025-03-25 21:07:23 +0300

    hw3  4-6

--- hw4/scripts/script.js
+++ hw4/scripts/script.js
@@ -70,7 +70,79 @@ function trimString(str, fromPosition, toPosition) {
     return str.substring(startIndex, endIndex);
   }
   
-  const originalString = "This is a test string with some words.";
+  const originalString = "Hello world!!!";
   
   const trimmedString1 = trimString(originalString, 5, 10);
   console.log(`Result of trimming - '${trimmedString1}'`);
+
+  //4
+  function getSumNumbers(number) 
+  {
+
+    if (typeof(number) !== 'number') {
+        return 'Error! Argument of getSumNumbers must be number';
+      }
+
+    const numberString = number.toString();
+
+    let sum = 0;
+
+    for (let i = 0; i < numberString.length; i++) {
+        sum += parseInt(numberString[i]); 
+    }
+  
+    return sum;
+  }
+  
+  console.log(getSumNumbers(2021));
+
+  //5
+  function getSum(a, b) {
+    if (a === b) {
+      return a;
+    }
+  
+    let sum = 0;
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  }
+  
+  console.log(getSum(1, 0));  
+  console.log(getSum(1, -5));  
+
+  //6
+  function fooBoo(boolValue, fooFunc, booFunc) {
+    if (typeof boolValue !== 'boolean') {
+      console.error("Error: boolValue must be a boolean.");
+      return;
+    }
+    if (typeof fooFunc !== 'function') {
+      console.error("Error: fooFunc must be a function.");
+      return;
+    }
+    if (typeof booFunc !== 'function') {
+      console.error("Error: booFunc must be a function.");
+      return;
+    }
+  
+    if (boolValue) {
+      fooFunc();
+    } else {
+      booFunc();
+    }
+  }
+  
+  function foo() {
+    console.log("foo");
+  }
+  
+  function boo() {
+    console.log("boo");
+  }
+  
+  fooBoo(false, foo, boo); 
