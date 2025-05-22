5784bafd003bbccb11f90ba52de8c86995c2361f
v.kisel <v.kisel@dpc-it.net>
2025-05-22 16:27:49 +0300

    ts1  done

---
+++ ts1/scriptTS.js
@@ -0,0 +1,27 @@
+var faculties = [
+    {
+        id: 1,
+        faculty: "History department",
+        subjects: ["The World History", "History of Rome"],
+        countStudents: 44
+    },
+    {
+        id: 2,
+        faculty: "Department of Biology",
+        subjects: ["biology", "chemistry"],
+        countStudents: 50
+    },
+    {
+        id: 3,
+        faculty: "Faculty of Mathematics",
+        subjects: ["mathematics", "geometry", "trigonometry"],
+        countStudents: 72
+    },
+    {
+        id: 4,
+        faculty: "Faculty of Design",
+        subjects: ["ui", "ux", "graphic design"],
+        countStudents: 37
+    }
+];
+console.log(faculties);
---
+++ ts1/scriptTS.ts
@@ -0,0 +1,35 @@
+interface IFaculty {
+    id: number;
+    faculty: string;
+    subjects: string[];
+    countStudents: number;
+}
+
+const faculties: IFaculty[] = [
+    {
+        id: 1,
+        faculty: "History department",
+        subjects: ["The World History", "History of Rome"],
+        countStudents: 44
+    },
+    {
+        id: 2,
+        faculty: "Department of Biology",
+        subjects: ["biology", "chemistry"],
+        countStudents: 50
+    },
+    {
+        id: 3,
+        faculty: "Faculty of Mathematics",
+        subjects: ["mathematics", "geometry", "trigonometry"],
+        countStudents: 72
+    },
+    {
+        id: 4,
+        faculty: "Faculty of Design",
+        subjects: ["ui", "ux", "graphic design"],
+        countStudents: 37
+    }
+];
+
+console.log(faculties)
